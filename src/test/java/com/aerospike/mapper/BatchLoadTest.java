0a33080c399b0f1af76b52f956340d4934f1606d
yrizhkov <yrizhkov@aerospike.com>
2021-04-20 16:10:48 +0300

    simplify the testBatchLoad logic; read-test a B object

--- src/test/java/com/aerospike/mapper/BatchLoadTest.java
+++ src/test/java/com/aerospike/mapper/BatchLoadTest.java
@@ -1,16 +1,17 @@
 package com.aerospike.mapper;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Test;
-
 import com.aerospike.mapper.annotations.AerospikeKey;
 import com.aerospike.mapper.annotations.AerospikeRecord;
 import com.aerospike.mapper.annotations.ParamFrom;
 import com.aerospike.mapper.tools.AeroMapper;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class BatchLoadTest extends AeroMapperBaseTest {
+
 	@AerospikeRecord(namespace = "test", set = "B")
 	public static class B {
 		@AerospikeKey
@@ -18,58 +19,58 @@ public class BatchLoadTest extends AeroMapperBaseTest {
 		public String name;
 		
 		public B(@ParamFrom("id") int id, @ParamFrom("name") String name) {
-			super();
 			this.id = id;
 			this.name = name;
 		}
 	}
-	
+
 	@AerospikeRecord(namespace = "test", set = "A")
 	public static class A {
 		@AerospikeKey
 		public int id;
 		public String name;
 		public List<B> data;
+
 		public A(int id, String name) {
-			super();
 			this.id = id;
 			this.name = name;
 			data = new ArrayList<>();
 		}
-		
-		public A addB(B b) {
-			data.add(b);
-			return this;
+
+		public void setBList(List<B> bees) {
+			data = bees;
 		}
 		
-		public A() {
-			super();
-		}
+		public A() {}
 	}
-	
+
 	@Test
 	public void testBatchLoad() {
 		AeroMapper mapper = new AeroMapper.Builder(client).build();
-		
+
 		B[] bees = new B[100];
 		for (int i = 0; i < 100; i++) {
 			bees[i] = new B(i, "B-" + i);
 		}
+
 		A[] as = new A[10];
 		for (int i = 0; i < 10; i++) {
-			as[i] = new A(100+i, "A-" + i);
-			for (int j = 0; j < i*8; j++) {
-				as[i].addB(bees[i+10+j]);
-			}
+			as[i] = new A(100 + i, "A-" + i);
+			as[i].setBList(Arrays.asList(
+					Arrays.copyOfRange(bees, i * 10, (i + 1) * 10)));
 		}
-		
+
 		mapper.save((Object[])bees);
 		mapper.save((Object[])as);
-		
+
+		System.out.println("--- Reading single object (bees[1]) ---");
+		B resultB = mapper.read(B.class, bees[1].id);
+		compare(bees[1], resultB);
+
 		System.out.println("--- Reading single object (a[1]) ---");
-		A result = mapper.read(A.class, as[1].id);
-		compare(as[1], result);
-		
+		A resultA = mapper.read(A.class, as[1].id);
+		compare(as[1], resultA);
+
 		System.out.println("--- Reading batch object with 6 keys ---");
 		A[] results = mapper.read(A.class, as[4].id, as[7].id, as[5].id, as[0].id, as[1].id, 3000);
 		compare(results[0], as[4]);
@@ -78,7 +79,6 @@ public class BatchLoadTest extends AeroMapperBaseTest {
 		compare(results[3], as[0]);
 		compare(results[4], as[1]);
 		compare(results[5], null);
-
 	}
-	
+
 }
