3f0a84bd161bc7edf12f0af428a3904eed33d8ac
roimenashe <roimenashe5@gmail.com>
2021-06-09 17:43:12 +0300

    Map Enums by short value fields instead of Enum constant names. + Add enum tests to both reactive and non-reactive flows.

--- src/main/java/com/aerospike/mapper/tools/mappers/EnumMapper.java
+++ src/main/java/com/aerospike/mapper/tools/mappers/EnumMapper.java
@@ -3,16 +3,29 @@ package com.aerospike.mapper.tools.mappers;
 import com.aerospike.client.AerospikeException;
 import com.aerospike.mapper.tools.TypeMapper;
 
+import java.lang.reflect.Field;
+
 public class EnumMapper extends TypeMapper {
 
 	private final Class<? extends Enum<?>> clazz;
-	
-	public EnumMapper(Class<? extends Enum<?>> clazz) {
+	private final String enumField;
+
+	public EnumMapper(Class<? extends Enum<?>> clazz, String enumField) {
 		this.clazz = clazz;
+		this.enumField = enumField;
 	}
 
 	@Override
 	public Object toAerospikeFormat(Object value) {
+		if (!enumField.equals("")) {
+			try {
+				Field enumRequestedField = clazz.getDeclaredField(enumField);
+				enumRequestedField.setAccessible(true);
+				return enumRequestedField.get(value).toString();
+			} catch (NoSuchFieldException | IllegalAccessException e) {
+				throw new AerospikeException("Cannot Map requested enum, issue with requested field.");
+			}
+		}
 		return value.toString();
 	}
 
@@ -21,14 +34,29 @@ public class EnumMapper extends TypeMapper {
 		if (value == null) {
 			return null;
 		}
-		String stringValue = (String)value;
-		Enum<?>[] constants=clazz.getEnumConstants();
-		for (Enum<?> thisEnum : constants) {
-			if (thisEnum.toString().equals(stringValue)) {
-				return thisEnum;
+
+		String stringValue = (String) value;
+		Enum<?>[] constants = clazz.getEnumConstants();
+
+		if (!enumField.equals("")) {
+			try {
+				Field enumRequestedField = clazz.getDeclaredField(enumField);
+				enumRequestedField.setAccessible(true);
+				for (Enum<?> thisEnum : constants) {
+					if (enumRequestedField.get(thisEnum).equals(stringValue)) {
+						return thisEnum;
+					}
+				}
+			} catch (NoSuchFieldException | IllegalAccessException e) {
+				throw new AerospikeException("Cannot Map requested enum, issue with requested field.");
+			}
+		} else {
+			for (Enum<?> thisEnum : constants) {
+				if (thisEnum.toString().equals(stringValue)) {
+					return thisEnum;
+				}
 			}
 		}
-		
 		throw new AerospikeException(String.format("Enum value of \"%s\" not found in type %s", stringValue, clazz.toString()));
 	}
 }
