168e2567a95e372c6854bd6694f4dc4716ac57e7
roimenashe <roimenashe5@gmail.com>
2021-10-07 14:45:39 +0300

    Revert latest commit - use @BeforeAll and add @AfterAll cleanup.

--- src/test/java/com/aerospike/mapper/reactive/ReactiveBatchLoadTest.java
+++ src/test/java/com/aerospike/mapper/reactive/ReactiveBatchLoadTest.java
@@ -7,17 +7,22 @@ import com.aerospike.mapper.annotations.AerospikeKey;
 import com.aerospike.mapper.annotations.AerospikeRecord;
 import com.aerospike.mapper.annotations.ParamFrom;
 import com.aerospike.mapper.tools.ReactiveAeroMapper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ReactiveBatchLoadTest extends ReactiveAeroMapperBaseTest {
 
     private static final String DATA_BIN = "data";
-    private final TestData testData = populateTestData();
+    private final B[] bees = new B[100];
+    private final A[] as = new A[10];
 
     @AerospikeRecord(namespace = "test", set = "B")
     public static class B {
@@ -52,20 +57,8 @@ public class ReactiveBatchLoadTest extends ReactiveAeroMapperBaseTest {
         }
     }
 
-    private static class TestData {
-        public B[] bees;
-        public A[] as;
-
-        public TestData(B[] bees, A[] as) {
-            this.bees = bees;
-            this.as = as;
-        }
-    }
-
-    private TestData populateTestData() {
-        B[] bees = new B[100];
-        A[] as = new A[10];
-
+    @BeforeAll
+    public void populateStaticData() {
         for (int i = 0; i < 100; i++) {
             bees[i] = new B(i, "B-" + i);
         }
@@ -75,8 +68,12 @@ public class ReactiveBatchLoadTest extends ReactiveAeroMapperBaseTest {
             as[i].setBList(Arrays.asList(
                     Arrays.copyOfRange(bees, i * 10, (i + 1) * 10)));
         }
+    }
 
-        return new TestData(bees, as);
+    @AfterAll
+    public void clear() {
+        client.truncate(null, "test", "A", null);
+        client.truncate(null, "test", "B", null);
     }
 
     private ReactiveAeroMapper populate() {
@@ -85,8 +82,8 @@ public class ReactiveBatchLoadTest extends ReactiveAeroMapperBaseTest {
 
         ReactiveAeroMapper reactiveMapper = new ReactiveAeroMapper.Builder(reactorClient).build();
 
-        reactiveMapper.save((Object[]) testData.bees).subscribeOn(Schedulers.parallel()).collectList().block();
-        reactiveMapper.save((Object[]) testData.as).subscribeOn(Schedulers.parallel()).collectList().block();
+        reactiveMapper.save((Object[]) bees).subscribeOn(Schedulers.parallel()).collectList().block();
+        reactiveMapper.save((Object[]) as).subscribeOn(Schedulers.parallel()).collectList().block();
 
         return reactiveMapper;
     }
@@ -95,18 +92,18 @@ public class ReactiveBatchLoadTest extends ReactiveAeroMapperBaseTest {
     public void testBatchLoad() {
         ReactiveAeroMapper reactiveMapper = populate();
 
-        B resultB = reactiveMapper.read(B.class, testData.bees[1].id).subscribeOn(Schedulers.parallel()).block();
-        compare(testData.bees[1], resultB);
+        B resultB = reactiveMapper.read(B.class, bees[1].id).subscribeOn(Schedulers.parallel()).block();
+        compare(bees[1], resultB);
 
-        A resultA = reactiveMapper.read(A.class, testData.as[1].id).subscribeOn(Schedulers.parallel()).block();
-        compare(testData.as[1], resultA);
+        A resultA = reactiveMapper.read(A.class, as[1].id).subscribeOn(Schedulers.parallel()).block();
+        compare(as[1], resultA);
 
         Integer[] ids = new Integer[6];
-        ids[0] = testData.as[4].id;
-        ids[1] = testData.as[7].id;
-        ids[2] = testData.as[5].id;
-        ids[3] = testData.as[0].id;
-        ids[4] = testData.as[1].id;
+        ids[0] = as[4].id;
+        ids[1] = as[7].id;
+        ids[2] = as[5].id;
+        ids[3] = as[0].id;
+        ids[4] = as[1].id;
         ids[5] = 3000;
 
         A[] results = new A[6];
@@ -115,11 +112,11 @@ public class ReactiveBatchLoadTest extends ReactiveAeroMapperBaseTest {
 
         assert resultsObjects != null;
         results = resultsObjects.toArray(results);
-        compare(results[0], testData.as[4]);
-        compare(results[1], testData.as[7]);
-        compare(results[2], testData.as[5]);
-        compare(results[3], testData.as[0]);
-        compare(results[4], testData.as[1]);
+        compare(results[0], as[4]);
+        compare(results[1], as[7]);
+        compare(results[2], as[5]);
+        compare(results[3], as[0]);
+        compare(results[4], as[1]);
         compare(results[5], null);
     }
 
@@ -127,18 +124,18 @@ public class ReactiveBatchLoadTest extends ReactiveAeroMapperBaseTest {
     public void testBatchLoadWithOperations() {
         ReactiveAeroMapper reactiveMapper = populate();
 
-        B resultB = reactiveMapper.read(B.class, testData.bees[1].id).subscribeOn(Schedulers.parallel()).block();
-        compare(testData.bees[1], resultB);
+        B resultB = reactiveMapper.read(B.class, bees[1].id).subscribeOn(Schedulers.parallel()).block();
+        compare(bees[1], resultB);
 
-        A resultA = reactiveMapper.read(A.class, testData.as[1].id).subscribeOn(Schedulers.parallel()).block();
-        compare(testData.as[1], resultA);
+        A resultA = reactiveMapper.read(A.class, as[1].id).subscribeOn(Schedulers.parallel()).block();
+        compare(as[1], resultA);
 
         Integer[] userKeys = new Integer[6];
-        userKeys[0] = testData.as[4].id;
-        userKeys[1] = testData.as[7].id;
-        userKeys[2] = testData.as[5].id;
-        userKeys[3] = testData.as[0].id;
-        userKeys[4] = testData.as[1].id;
+        userKeys[0] = as[4].id;
+        userKeys[1] = as[7].id;
+        userKeys[2] = as[5].id;
+        userKeys[3] = as[0].id;
+        userKeys[4] = as[1].id;
         userKeys[5] = 3000;
 
         Operation[] ops = new Operation[2];
@@ -151,15 +148,15 @@ public class ReactiveBatchLoadTest extends ReactiveAeroMapperBaseTest {
         results = resultsList.toArray(results);
 
         compare(results[0].data.get(0).id, 10);
-        compare(results[0].data.get(1).id, testData.as[4].data.get(testData.as[4].data.size() - 1).id);
+        compare(results[0].data.get(1).id, as[4].data.get(as[4].data.size() - 1).id);
         compare(results[1].data.get(0).id, 10);
-        compare(results[1].data.get(1).id, testData.as[7].data.get(testData.as[7].data.size() - 1).id);
+        compare(results[1].data.get(1).id, as[7].data.get(as[7].data.size() - 1).id);
         compare(results[2].data.get(0).id, 10);
-        compare(results[2].data.get(1).id, testData.as[5].data.get(testData.as[5].data.size() - 1).id);
+        compare(results[2].data.get(1).id, as[5].data.get(as[5].data.size() - 1).id);
         compare(results[3].data.get(0).id, 10);
-        compare(results[3].data.get(1).id, testData.as[0].data.get(testData.as[0].data.size() - 1).id);
+        compare(results[3].data.get(1).id, as[0].data.get(as[0].data.size() - 1).id);
         compare(results[4].data.get(0).id, 10);
-        compare(results[4].data.get(1).id, testData.as[1].data.get(testData.as[1].data.size() - 1).id);
+        compare(results[4].data.get(1).id, as[1].data.get(as[1].data.size() - 1).id);
         compare(results[5], null);
     }
 }
