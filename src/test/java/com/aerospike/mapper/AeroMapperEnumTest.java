8da6b649b28ed8613328da4b082eb07a08b14f1a
Eugene R <yrizhkov@aerospike.com>
2022-09-19 11:22:31 +0300

    Handle null values in EnumMapper (#122)

--- src/test/java/com/aerospike/mapper/AeroMapperEnumTest.java
+++ src/test/java/com/aerospike/mapper/AeroMapperEnumTest.java
@@ -1,15 +1,49 @@
 package com.aerospike.mapper;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
-
 import com.aerospike.mapper.annotations.AerospikeEnum;
 import com.aerospike.mapper.annotations.AerospikeKey;
 import com.aerospike.mapper.annotations.AerospikeRecord;
 import com.aerospike.mapper.tools.AeroMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AeroMapperEnumTest extends AeroMapperBaseTest {
+
+    @Test
+    public void runTest() {
+        A a1 = new A(1, "a", 10, Status.MARRIED, Country.ARGENTINA, Country.ARGENTINA);
+        A a2 = new A(2, "b", 20, Status.SINGLE, Country.DENMARK, Country.DENMARK);
+        A a3 = new A(3, "c", 30, Status.COMPLICATED, Country.UNITED_STATES, Country.UNITED_STATES);
+        A a4 = new A(4, "d", 40, null, null, null);
+
+        AeroMapper mapper = new AeroMapper.Builder(client).build();
+        mapper.save(a1);
+        mapper.save(a2);
+        mapper.save(a3);
+        mapper.save(a4);
+
+        A a11 = mapper.read(A.class, 1);
+        A a12 = mapper.read(A.class, 2);
+        A a13 = mapper.read(A.class, 3);
+        A a14 = mapper.read(A.class, 4);
+
+        assertEquals(a1.id, a11.id);
+        assertEquals(a1.status, a11.status);
+        assertEquals(a1.country, a11.country);
+
+        assertEquals(a2.name, a12.name);
+        assertEquals(a2.country, a12.country);
+        assertEquals(a2.countryAnno, a12.countryAnno);
+
+        assertEquals(a3.age, a13.age);
+        assertEquals(a3.country, a13.country);
+        assertEquals(a3.countryAnno, a13.countryAnno);
+
+        assertEquals(a4.age, a14.age);
+        assertEquals(a4.country, a14.country);
+        assertEquals(a4.countryAnno, a14.countryAnno);
+    }
 
     enum Status {
         MARRIED,
@@ -41,10 +75,9 @@ public class AeroMapperEnumTest extends AeroMapperBaseTest {
         public String name;
         public int age;
         public Status status;
-        public Country country;
         @AerospikeEnum(enumField = "countryCode")
         public Country countryAnno;
-
+        private Country country;
 
         public A() {
         }
@@ -58,30 +91,5 @@ public class AeroMapperEnumTest extends AeroMapperBaseTest {
             this.country = country;
             this.countryAnno = countryAnno;
         }
-    }
-
-    @Test
-    public void runTest() {
-        A a1 = new A(1, "a", 10, Status.MARRIED, Country.ARGENTINA, Country.ARGENTINA);
-        A a2 = new A(2, "b", 20, Status.SINGLE, Country.DENMARK, Country.DENMARK);
-        A a3 = new A(3, "c", 30, Status.COMPLICATED, Country.UNITED_STATES, Country.UNITED_STATES);
-
-        AeroMapper mapper = new AeroMapper.Builder(client).build();
-        mapper.save(a1);
-        mapper.save(a2);
-        mapper.save(a3);
-
-        A a11 = mapper.read(A.class, 1);
-        A a12 = mapper.read(A.class, 2);
-        A a13 = mapper.read(A.class, 3);
-
-        assertEquals(a1.id, a11.id);
-        assertEquals(a1.status, a11.status);
-        assertEquals(a1.country, a11.country);
-        assertEquals(a2.name, a12.name);
-        assertEquals(a2.country, a12.country);
-        assertEquals(a2.countryAnno, a12.countryAnno);
-        assertEquals(a3.age, a13.age);
-        assertEquals(a3.countryAnno, a13.countryAnno);
     }
 }
