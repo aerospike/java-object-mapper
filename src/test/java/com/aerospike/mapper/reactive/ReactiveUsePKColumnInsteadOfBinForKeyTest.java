954fac9e99d230f35b96a92671bbd4d4f5b8222f
Tim F <120514393+tim-aero@users.noreply.github.com>
2024-07-01 09:33:22 -0700

    FMWK-468 Allow send key classes to use PK instead of Bin (#160)
    * Allowed the use of the Aerospike Key instead of re-writing a key field.
    
    Added a new annotation field "storeInPkOnly" to use the PK field as
    storing the key, rather than explicitly setting a bin in the database.
    - Added unit tests for the same, both reactive and normal
    - Tested with annotations, YAML config, code config.
    
    * Rolled back accidental push of wrong connection port.
    
    * Format
    
    * Cleanup and format
    
    * Updated field name to storeAsBin
    
    Changed the config field name from `storeInPkOnly` to `storeAsBin`,
    added unit test to ensure config validation works.
    
    * Rolled back test port to port 3000
    
    * Remove old "storeInPkOnly" terminology, add javadocs, cleanup
    
    * Apply suggestions from code review
    
    Co-authored-by: Eugene R. <yrizhkov@aerospike.com>
    
    * Revert change, causing NPE on runTestViaConfig() test
    
    ---------
    
    Co-authored-by: roimenashe <roimenashe5@gmail.com>
    Co-authored-by: Roi Menashe <33356310+roimenashe@users.noreply.github.com>
    Co-authored-by: Eugene R. <yrizhkov@aerospike.com>

---
+++ src/test/java/com/aerospike/mapper/reactive/ReactiveUsePKColumnInsteadOfBinForKeyTest.java
@@ -0,0 +1,94 @@
+package com.aerospike.mapper.reactive;
+
+import com.aerospike.client.query.KeyRecord;
+import com.aerospike.mapper.annotations.AerospikeKey;
+import com.aerospike.mapper.annotations.AerospikeRecord;
+import com.aerospike.mapper.tools.ReactiveAeroMapper;
+import com.aerospike.mapper.tools.configuration.ClassConfig;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+import reactor.core.scheduler.Schedulers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReactiveUsePKColumnInsteadOfBinForKeyTest extends ReactiveAeroMapperBaseTest {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @AerospikeRecord(namespace = "test", set = "testSet", sendKey = true)
+    public static class A {
+        @AerospikeKey(storeAsBin = false)
+        private long key1;
+        private String value;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class B {
+        private String keyStr;
+        private String value;
+    }
+
+    @Test
+    public void runTest() {
+        ReactiveAeroMapper reactiveMapper = new ReactiveAeroMapper.Builder(reactorClient).build();
+        A a = new A(1, "test");
+        reactiveMapper.save(a).subscribeOn(Schedulers.parallel()).block();
+
+        A readA = reactiveMapper.read(A.class, 1).subscribeOn(Schedulers.parallel()).block();
+        assertNotNull(readA);
+        assertEquals(1, readA.key1);
+
+        KeyRecord rawObject = reactorClient.get(null, reactiveMapper.getRecordKey(a).block()).block();
+        assertNotNull(rawObject);
+        assertFalse(rawObject.record.bins.containsKey("key1"));
+    }
+
+    @Test
+    public void runTestViaYaml() throws Exception {
+        String yaml = "---\n" +
+                "classes:\n" +
+                " - class: com.aerospike.mapper.reactive.ReactiveUsePKColumnInsteadOfBinForKeyTest$B\n" +
+                "   namespace: test\n" +
+                "   set: testSet\n" +
+                "   sendKey: true\n" +
+                "   key:\n" +
+                "     field: keyStr\n" +
+                "     storeAsBin: false\n";
+        ReactiveAeroMapper reactiveMapper = new ReactiveAeroMapper.Builder(reactorClient).withConfiguration(yaml).build();
+        B b = new B("key1", "test1");
+        reactiveMapper.save(b).subscribeOn(Schedulers.parallel()).block();
+
+        B readB = reactiveMapper.read(B.class, "key1").subscribeOn(Schedulers.parallel()).block();
+        assertNotNull(readB);
+        assertEquals("key1", readB.keyStr);
+
+        KeyRecord rawObject = reactorClient.get(null, reactiveMapper.getRecordKey(b).block()).block();
+        assertNotNull(rawObject);
+        assertFalse(rawObject.record.bins.containsKey("keyStr"));
+    }
+
+    @Test
+    public void runTestViaConfig() {
+        ClassConfig classBConfig = new ClassConfig.Builder(B.class)
+                .withNamespace("test")
+                .withSet("testSet")
+                .withSendKey(true)
+                .withKeyFieldAndStoreAsBin("keyStr", false).build();
+        ReactiveAeroMapper reactiveMapper = new ReactiveAeroMapper.Builder(reactorClient).withClassConfigurations(classBConfig).build();
+        B b = new B("key2", "test2");
+        reactiveMapper.save(b).subscribeOn(Schedulers.parallel()).block();
+
+        B readB = reactiveMapper.read(B.class, "key2").subscribeOn(Schedulers.parallel()).block();
+        assertNotNull(readB);
+        assertEquals("key2", readB.keyStr);
+
+        KeyRecord rawObject = reactorClient.get(null, reactiveMapper.getRecordKey(b).block()).block();
+        assertNotNull(rawObject);
+        assertFalse(rawObject.record.bins.containsKey("keyStr"));
+    }
+}
